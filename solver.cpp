12c1bdc1e9280175d686e63d36a48aee1c912229
Shani <shanishalel89@gmail.com>
2020-04-24 14:28:32 +0300

    start the ex.

--- solver.cpp
+++ solver.cpp
@@ -4,4 +4,38 @@
 #include <complex>
 
 using namespace std;
+using namespace solver;
+
+class solve{
+
+private:
+  static double _a,_b,_c;
+  solve(){
+      _a=0;
+      _b=0;
+      _c=0;
+  }
+
+public:
+    solve operator* (double b, RealVariable x){
+    _b=b;
+    return *this;
+    }
+
+    //4*x^2
+     solve operator^ (RealVariable x){
+        _a=solve::_b;
+        _b=0;
+    }
+
+    //x^2+4
+    solve operator+ (int c){
+    _c=c;
+    }
+
+
+
+};
+
+
 
--- solver.hpp
+++ solver.hpp
@@ -6,50 +6,92 @@
 using namespace std;
 namespace solver{
 
+
+    class RealVariable{
+    public:
+    double x;
+
+        RealVariable(){
+            x=0;
+        }
+
+    };
+
+ 
+    class ComplexVariable{
+        public:
+        double _im,_re;
+
+        ComplexVariable(){
+            _im=0;
+            _re=0;
+        }
+
+    };
+
+
+    class solve{
+        public:
+        double _a,_b,_c;
+        
+        
+         friend solve operator* (double b,RealVariable x);
+         solve operator^ (RealVariable x);
+         solve operator+ (int c);
+    };
+};
+
 //4*x
-class solve{
+// class solve{
 
-private:
-  static double _a,_b,_c;
-  solve(){
-      _a=0;
-      _b=0;
-      _c=0;
-  }
+// private:
+//   static double _a,_b,_c;
+//   solve(){
+//       _a=0;
+//       _b=0;
+//       _c=0;
+//   }
 
-public:
-    solve operator* (double b){
-    solve::_b=b;
-    }
+// public:
+//     solve operator* (double b){
+//     solve::_b=b;
+//     }
 
-//4*x^2
-     solve operator^ (RealVariable x){
-        solve::_a=solve::_b;
-        solve::_b=0;
-    }
+//     //4*x^2
+//      solve operator^ (RealVariable x){
+//         solve::_a=solve::_b;
+//         solve::_b=0;
+//     }
 
-
-
-solve operator+ (RealVariable x);
-solve operator+ (ComplexVariable y);
-solve operator* (ComplexVariable y);
-solve operator^ (int n);
-
-};
+//     //x^2+4
+//     solve operator+ (int c){
+//     solve::_c=c;
+//     }
 
 
+// solve operator+ (ComplexVariable y);
+// solve operator* (ComplexVariable y);
+// solve operator^ (int n);
 
-class RealVariable{
-  
+// };
 
 
 
-};
+// class RealVariable{
+//   double x;
 
-class ComplexVariable{
+//   RealVariable(){
+//       x=0;
+//   }
+
+
+
+// };
+
+// class ComplexVariable{
 
 
 
 
-};
-};
+// };
+// };
