a156278b4761e25e4b85b79c2ce13c3d47434c30
darsam44 <darsam44@gmail.com>
2020-04-26 21:10:20 +0300

    try to fix

--- solver.cpp
+++ solver.cpp
@@ -14,6 +14,72 @@ double solve (RealVariable x) {
     return 1;
 }
 
+///// '*' //////
+ RealVariable &operator* (double n , RealVariable &x);
+ RealVariable &operator* (RealVariable &x , double n);
 
+///// '/' //////
+ RealVariable &operator/ (RealVariable &x_1 , RealVariable &x_2);
+ RealVariable &operator/ (RealVariable &x , double n);
+ RealVariable &operator/ ( double n ,RealVariable &x);
+
+///// '+' //////
+ RealVariable &operator+ (RealVariable &x_1 , RealVariable &x_2);
+ RealVariable &operator+ (RealVariable &x , double n);
+ RealVariable &operator+ ( double n ,RealVariable &x);
+
+///// '-' //////
+ RealVariable &operator- (RealVariable &x_1 , RealVariable &x_2);
+ RealVariable &operator- (RealVariable &x , double n);
+ RealVariable &operator- ( double n ,RealVariable &x);
+
+///// '^' //////
+ RealVariable &operator^ (RealVariable &x , int n);
+
+///// '==' //////
+ RealVariable &operator== (RealVariable &x_1 , RealVariable &x_2);
+ RealVariable &operator== (RealVariable &x , double n);
+ RealVariable &operator== ( double n ,RealVariable &x);
+
+
+////// Complex ///////
+
+///// '*' //////
+ ComplexVariable &operator* (ComplexVariable &y , std::complex<double> n);
+ ComplexVariable &operator* ( std::complex<double> n ,ComplexVariable &y);
+ ComplexVariable &operator* (ComplexVariable &y , double n);
+ ComplexVariable &operator* ( double n ,ComplexVariable &y);
+
+
+///// '/' //////
+ ComplexVariable &operator/ (ComplexVariable &y_1 , ComplexVariable &y_2);
+ ComplexVariable &operator/ (ComplexVariable &y , std::complex<double> n);
+ ComplexVariable &operator/ ( std::complex<double> n ,ComplexVariable &y);
+ ComplexVariable &operator/ (ComplexVariable &y , double n);
+ ComplexVariable &operator/ ( double n ,ComplexVariable &y);
+
+///// '+' //////
+ ComplexVariable &operator+ (ComplexVariable &y_1 , ComplexVariable &y_2);
+ ComplexVariable &operator+ (ComplexVariable &y , std::complex<double> n);
+ ComplexVariable &operator+ ( std::complex<double> n ,ComplexVariable &y);
+ ComplexVariable &operator+ (ComplexVariable &y , double n);
+ ComplexVariable &operator+ ( double n ,ComplexVariable &y);
+
+///// '-' //////
+ ComplexVariable &operator- (ComplexVariable &y_1 , ComplexVariable &y_2);
+ ComplexVariable &operator- (ComplexVariable &y , std::complex<double> n);
+ ComplexVariable &operator- ( std::complex<double> n ,ComplexVariable &y);
+ ComplexVariable &operator- (ComplexVariable &y , double n);
+ ComplexVariable &operator- ( double n ,ComplexVariable &y);
+
+///// '^' //////
+ ComplexVariable &operator^ (ComplexVariable &y , int n);
+
+///// '==' //////
+ ComplexVariable &operator== (ComplexVariable &y_1 , ComplexVariable &y_2);
+ ComplexVariable &operator== (ComplexVariable &y , std::complex<double> n);
+ ComplexVariable &operator== ( std::complex<double> n ,ComplexVariable &y);
+ ComplexVariable &operator== (ComplexVariable &y , double n);
+ ComplexVariable &operator== ( double n ,ComplexVariable &y);
 
 
